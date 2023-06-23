4f91ca25b3d216840d3920993de545707e93172d
Oscar <oscarmappley@icloud.com>
2023-06-23 23:32:55 +0100

    find me thing fully working

--- js/script.js
+++ js/script.js
@@ -1,3 +1,25 @@
 function goHome() {
     window.location.href="index.html";
 }
+
+function showFindMe() {
+    var element = document.querySelector('.dropdown-content');
+    element.style.display = 'block';
+
+    let referenceElement = document.getElementsByClassName('dropdown')[0];
+
+    let referenceRect = referenceElement.getBoundingClientRect();
+    let referenceTop = referenceRect.top;
+    let referenceLeft = referenceRect.left;
+
+    referenceTop += 23;
+
+    element.style.position = 'absolute';
+    element.style.top = referenceTop + 'px';
+    element.style.left = referenceLeft + 'px';
+}
+  
+function hideFindMe() {
+    var element = document.querySelector('.dropdown-content');
+    element.style.display = 'none';
+}
